b390f9dcd3acf5f834f4d438ea1f2e11b536419b
karachungen <karachungen@gmail.com>
2014-05-29 14:43:39 +0300

    new  world opened

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,8 +19,8 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+
+	        $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
